cc7003eff8d1bca2f0fd4cb12fbdfeb053597421
ganbruce <923099645@qq.com>
2018-10-24 23:27:42 +0800

    添加创建数据库的脚本

---
+++ sql/database_init.sql
@@ -0,0 +1,26 @@
+
+#===================
+## 创建数据库脚本
+#===================
+
+DROP DATABASE IF EXISTS `bbs-admin`;
+CREATE database `bbs-admin` CHARACTER set UTF8;
+
+DROP DATABASE IF EXISTS `bbs-forum`;
+CREATE database `bbs-forum` CHARACTER set UTF8;
+
+DROP DATABASE IF EXISTS `bbs-log`;
+CREATE database `bbs-log` CHARACTER set UTF8;
+
+DROP DATABASE IF EXISTS `bbs-notice`;
+CREATE database `bbs-notice` CHARACTER set UTF8;
+
+DROP DATABASE IF EXISTS `bbs-post`;
+CREATE database `bbs-post` CHARACTER set UTF8;
+
+DROP DATABASE IF EXISTS `bbs-reply`;
+CREATE database `bbs-reply` CHARACTER set UTF8;
+
+DROP DATABASE IF EXISTS `bbs-user`;
+CREATE database `bbs-user` CHARACTER set UTF8;
+
