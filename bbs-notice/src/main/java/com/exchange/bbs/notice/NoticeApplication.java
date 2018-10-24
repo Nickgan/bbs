c2ecb531fc6485a06b143741124914d1eba19fae
ganbruce <923099645@qq.com>
2018-10-24 15:49:04 +0800

    test

---
+++ bbs-notice/src/main/java/com/exchange/bbs/notice/NoticeApplication.java
@@ -0,0 +1,17 @@
+package com.exchange.bbs.notice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author gan
+ * @date 2018/10/24 下午3:47
+ */
+
+@SpringBootApplication
+public class NoticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoticeApplication.class, args);
+    }
+}
--- bbs-notice/src/main/java/com/exchange/bbs/notice/controller/NoticeController.java
+++ bbs-notice/src/main/java/com/exchange/bbs/notice/controller/NoticeController.java
@@ -1,9 +1,19 @@
 package com.exchange.bbs.notice.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author gan
  * @date 2018/10/24 下午3:44
  */
+
+@RestController
 public class NoticeController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
 
 }
