6618bc4cfa94c7e63193eb97a7a45abefbc500fa
ganbruce <923099645@qq.com>
2018-10-25 14:16:31 +0800

    用户模块服务注册

--- bbs-user/src/main/java/com/exchange/bbs/user/UserApplication.java
+++ bbs-user/src/main/java/com/exchange/bbs/user/UserApplication.java
@@ -3,6 +3,7 @@ package com.exchange.bbs.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * bbs系统用户
@@ -13,6 +14,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
 @EntityScan("com.exchange.bbs.entity.user")
+@EnableDiscoveryClient
 public class UserApplication {
 
     public static void main(String[] args) {
