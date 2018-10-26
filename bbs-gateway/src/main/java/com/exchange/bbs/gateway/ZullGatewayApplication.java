344bdf872ea7ee5618654b09037361a83494a45b
ganbruce <923099645@qq.com>
2018-10-26 16:02:52 +0800

    加入网关模块

---
+++ bbs-gateway/src/main/java/com/exchange/bbs/gateway/ZullGatewayApplication.java
@@ -0,0 +1,23 @@
+package com.exchange.bbs.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * bbs 网关服务
+ *
+ * @author gan
+ * @date 2018/10/26 下午3:55
+ */
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class ZullGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZullGatewayApplication.class, args);
+    }
+}
---
+++ bbs-gateway/src/main/java/com/exchange/bbs/gateway/filter/ZullFilter.java
@@ -0,0 +1,11 @@
+package com.exchange.bbs.gateway.filter;
+
+/**
+ * zull过滤器
+ *
+ * @author gan
+ * @date 2018/10/26 下午4:00
+ */
+public class ZullFilter {
+
+}
