e30d74665a983e0e8068f2a869928670d62c3263
ganbruce <923099645@qq.com>
2018-11-04 21:47:06 +0800

    eureka 高可用

--- bbs-eureka-server-second/src/main/java/com/exchange/bbs/eureka/EurekaServerSecondApplication.java
+++ bbs-eureka-server-second/src/main/java/com/exchange/bbs/eureka/EurekaServerSecondApplication.java
@@ -13,10 +13,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaServerApplication {
+public class EurekaServerSecondApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaServerApplication.class, args);
+        SpringApplication.run(EurekaServerSecondApplication.class, args);
     }
 
 }
