3e6247f7f029a2484592c23705fab817e903791a
ganbo <923099645@qq.com>
2019-04-12 19:39:44 +0800

    nothing

--- bbs-admin/bbs-admin-web/src/main/java/com/exchange/bbs/admin/controller/AdminUserController.java
+++ bbs-admin/bbs-admin-web/src/main/java/com/exchange/bbs/admin/controller/AdminUserController.java
@@ -1,10 +1,12 @@
 package com.exchange.bbs.admin.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author gan
  * @date 2018/10/24 下午7:39
  */
-
+@RestController
 public class AdminUserController {
 
 }
